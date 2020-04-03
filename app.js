e6ca918c966354deb7535244ddf0f8762466883f
yeffry <yeffry45@gmail.com>
2020-04-03 19:58:10 -0400

    updated renderBudget to create and append text

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 class Budget {
-    constructor(form, budgetP, incomeP, expensesP) {
-        this.form = form
-        this.budgetDOM = budgetP
-        this.incomeDOM = incomeP
-        this.expenseDOM = expensesP
+    constructor(formDOM, budgetDOM, incomeDOM, expenseDOM, incomeTabDOM, expenseTabDOM) {
+        this.formDOM = formDOM
+        this.budgetDOM = budgetDOM
+        this.incomeDOM = incomeDOM
+        this.expenseDOM = expenseDOM
+        this.incomeTabDOM = incomeTabDOM
+        this.expenseTabDOM = expenseTabDOM
         this.totalIncome = 0
         this.totalExpenses = 0
         this.budget = 0
         this.income = []
         this.expenses = []
 
-        this.form.addEventListener('submit', this.onSubmit);
+        this.formDOM.addEventListener('submit', this.onSubmit);
     }
     onSubmit = (e) => {
         e.preventDefault();
-        this.option = e.target.optionValue.value;
+        let description = e.target.description.value;
+        let amount = parseInt(e.target.amount.value);
+
+        this.option = e.target.optionValue.value
+
         if (this.option === '+') {
             this.income.push({
-                description: e.target.description.value,
-                amount: parseFloat(e.target.amount.value),
+                description,
+                amount,
             })
             this.addToBudget();
         } else {
             this.expenses.push({
-                description: e.target.description.value,
-                amount: parseFloat(e.target.amount.value),
+                description,
+                amount,
             });
             this.addToExpense()
-
         }
-        e.target.reset();
+        
+        e.target.description.value = '';
+        e.target.amount.value = '';
+
+
     };
     addToBudget = () => {
         this.totalIncome = this.income.reduce((acc, curr) => {
@@ -42,26 +51,45 @@ class Budget {
         this.totalExpenses = this.expenses.reduce((acc, curr) => {
             return acc = acc + curr.amount;
         }, 0);
-        console.log(this.totalExpenses)
         this.calcBudget();
 
     };
     calcBudget = () => {
     
         this.budget = this.totalIncome - this.totalExpenses;
-        this.render();
+        this.renderBudget();
     };
-    render = () => {
+    renderBudget = () => {
         this.incomeDOM.textContent = this.totalIncome;
         this.expenseDOM.textContent = this.totalExpenses
         this.budgetDOM.textContent = this.budget;
 
+        this.income.map((income) => {
+            const div = document.createElement('div');
+            const para = document.createElement('p');
+
+            para.textContent = income.description;
+            this.incomeTabDOM.appendChild(para);
+
+            const para2 = document.createElement('p');
+            para2.textContent = income.amount;
+            div.appendChild(para);
+            div.appendChild(para2)
+            this.incomeTabDOM.appendChild(div);
+        })
+
+
+
+
     };
 };
 
-const form = document.querySelector('form');
-const budgetP = document.querySelector('.budget-amount');
-const incomeP = document.querySelector('.income');
-const expensesP = document.querySelector('.expenses');
 
-const userBudget = new Budget(form, budgetP, incomeP, expensesP);
+const formDOM = document.querySelector('.form');
+const budgetDOM = document.querySelector('.budget-amount');
+const incomeDOM = document.querySelector('.income');
+const expenseDOM = document.querySelector('.expenses');
+const incomeList = document.querySelector('.income-list');
+const expenseList = document.querySelector('.expense-list');
+
+const userBudget = new Budget(formDOM, budgetDOM, incomeDOM, expenseDOM, incomeList, expenseList);
